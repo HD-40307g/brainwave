f4c3899520232fdc59732939a4887d7bffab2657
HD-40307g <152268341+HD-40307g@users.noreply.github.com>
2024-11-18 18:27:21 +0100

    Refactor Button.jsx component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import ButtonSvg from "../assets/svg/ButtonSvg";
+import PropTypes from 'prop-types';
 
 const Button = ({ className, href, onClick, children, px, white }) => {
   const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${px || "px-7"} ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
   const spanClasses = "relative z-10";
 
   const renderButton = () => (
-    <button className={classes} onClick={onClick}>
+    <button className={classes} onClick={onClick} aria-label={children}>
       <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </button>
   );
 
   const renderLink = () => (
-    <a href={href} className={classes}>
+    <a href={href} className={classes} aria-label={children}>
       <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </a>
@@ -22,4 +23,21 @@ const Button = ({ className, href, onClick, children, px, white }) => {
   return href ? renderLink() : renderButton();
 };
 
-export default Button;
+Button.propTypes = {
+  className: PropTypes.string,
+  href: PropTypes.string,
+  onclick: PropTypes.func,
+  children: PropTypes.node.isRequired,
+  px: PropTypes.string,
+  white: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  className: '',
+  href: null,
+  onclick: null,
+  px: 'px-7',
+  white: false,
+};
+
+export default React.memo(Button);
