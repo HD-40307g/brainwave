c8b6ae85bdfdecec425ee00fa7badd90396f2458
HD-40307g <152268341+HD-40307g@users.noreply.github.com>
2024-11-19 22:06:00 +0100

    Refactor Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, memo } from "react";
 import { useLocation } from "react-router-dom";
 import { brainwave } from "../assets";
 import { navigation } from "../constants";
@@ -43,20 +43,20 @@ const Header = () => {
           } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
         >
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
-            {navigation.map((item) => (
+            {navigation.map(({url, id, onlyMobile, title}) => (
               <a
-                href={item.url}
-                key={item.id}
+                href={url}
+                key={id}
                 onClick={handleClick}
                 className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
-                  item.onlyMobile ? "lg:hidden" : ""
+                  onlyMobile ? "lg:hidden" : ""
                 } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg-font-semibold ${
-                  item.rul === pathname.hash
+                  url === pathname.hash
                     ? "z-2 lg:text-n-1"
                     : "lg:tet-n-1/50"
                 } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
               >
-                {item.title}
+                {title}
               </a>
             ))}
           </div>
@@ -83,4 +83,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default memo(Header);
