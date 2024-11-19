a46a7e51222120659da8bab724e092f81d5ed109
HD-40307g <152268341+HD-40307g@users.noreply.github.com>
2024-11-19 21:16:47 +0100

    Refactor Collaboration.jsx component

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -1,10 +1,8 @@
-import React from "react";
+import React, { memo } from "react";
 import Section from "./Section";
 import Button from "./Button";
-import { check } from "../assets";
-import { collabApps, collabContent } from "../constants";
-import { collabText } from "../constants";
-import { brainwaveSymbol } from "../assets";
+import { check, brainwaveSymbol } from "../assets";
+import { collabApps, collabContent, collabText } from "../constants";
 import { LeftCurve, RightCurve } from "./design/Collaboration";
 
 const Collaboration = () => {
@@ -12,28 +10,31 @@ const Collaboration = () => {
     <Section crosses>
       <div className="container lg:flex">
         <div className="max-w-[25rem]">
+          {/* Heading */}
           <h2 className="h2 mb-4 md:mb-8">
             AI Chat App for seamless collaboration
           </h2>
+          {/* Collaboration Content List */}
           <ul className='max-w-[22rem] mb-10 md"mb-14'>
-            {collabContent.map((item) => (
-              <li className="mb-3 py-3" key={item.id}>
+            {collabContent.map(({ id, title, text }) => (
+              <li className="mb-3 py-3" key={id}>
                 <div className="flex items-center">
                   <img src={check} alt="check" width={24} height={24} />
-                  <h6 className="body-2 ml-5">{item.title}</h6>
+                  <h6 className="body-2 ml-5">{title}</h6>
                 </div>
-                {item.text && (
-                  <p className="body-2 mt-3 text-n-4">{item.text}</p>
-                )}
+                {text && <p className="body-2 mt-3 text-n-4">{text}</p>}
               </li>
             ))}
           </ul>
+          {/* Call to Action Button */}
           <Button>Try it now</Button>
         </div>
         <div className="lg:ml-auto xl:w-[38rem] mt-4">
+          {/* Collaboration Text */}
           <p className="body-2 mb-8 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto">
             {collabText}
           </p>
+          {/* Collaboration Apps */}
           <div className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
             <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
               <div className="w-[6rem] aspect-square m-auto p-[0.2rem] bg-conic-gradient rounded-full">
@@ -80,4 +81,4 @@ const Collaboration = () => {
   );
 };
 
-export default Collaboration;
+export default memo(Collaboration);
