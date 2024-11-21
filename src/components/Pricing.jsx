b6dcb85f88e252e276003d4296af21e58500a4ba
HD-40307g <152268341+HD-40307g@users.noreply.github.com>
2024-11-21 10:12:05 +0100

    Refactor Pricing.jsx

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import Section from "./Section";
 import Heading from "./Heading";
 import PricingList from "./PricingList";
@@ -12,18 +13,20 @@ const Pricing = () => {
         <div className="hidden relative justify-center mb-[6.5rem] lg:flex">
           <img
             src={smallSphere}
-            alt="Shpere"
+            alt="Sphere"
             className="relative z-1"
             width={255}
             height={255}
+            loading="lazy"
           />
-          <div className="absolute top=1/2 left-1/2 w-[60rem] -translate-x-1/2 -translate-y-1/2 pointer-events-none">
+          <div className="absolute top-1/2 left-1/2 w-[60rem] -translate-x-1/2 -translate-y-1/2 pointer-events-none">
             <img
               src={stars}
               alt="Stars"
               className="w-full"
               width={950}
               height={400}
+              loading="lazy"
             />
           </div>
         </div>
@@ -49,4 +52,12 @@ const Pricing = () => {
   );
 };
 
-export default Pricing;
+Pricing.propTypes = {
+  className: PropTypes.string,
+};
+
+Pricing.defaultProps = {
+  className: "",
+};
+
+export default React.memo(Pricing);
