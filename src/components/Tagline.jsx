e982dae295c2e1a80cec44438548eb42536b0bb5
HD-40307g <152268341+HD-40307g@users.noreply.github.com>
2024-11-21 11:27:21 +0100

    Refactor Tagline.jsx

--- src/components/Tagline.jsx
+++ src/components/Tagline.jsx
@@ -1,14 +1,29 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import brackets from "../assets/svg/Brackets";
 
-const Tagline = ({ className, children }) => {
+const Tagline = ({ className = "", children }) => {
   return (
-    <div className={`tagline flex items-center ${className || ""}`}>
+    <div className={`tagline flex items-center ${className}`}>
+      {/* Left bracket */}
       {brackets("left")}
+      {/* Tagline text */}
       <div className="mx-3 text-n-3">{children}</div>
+      {/* Right bracket */}
       {brackets("right")}
     </div>
   );
 };
 
-export default Tagline;
+//Default props
+Tagline.defaultProps = {
+  className: "",  
+}
+
+//Prop validation
+Tagline.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
+
+export default React.memo(Tagline);
